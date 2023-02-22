20e51da7c005da469e9cf35122ef2413d5c40f1f
Mustofaadni04040 <adniadni04@gmail.com>
2023-02-22 14:35:04 +0700

    Craate parts Testimony and component  star

---
+++ src/elements/Star/index.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+import propTypes from "prop-types";
+
+import "./index.scss";
+
+export default function Star({ className, value, height, width, spacing }) {
+  const decimals = Number(value) % 1;
+
+  const star = [];
+  let leftPos = 0;
+
+  for (let index = 0; index < 5 && index < value - decimals; index++) {
+    leftPos = leftPos + width;
+    star.push(
+      //full star
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing,
+        }}
+      ></div>
+    );
+  }
+
+  if (decimals > 0 && value <= 5) {
+    star.push(
+      //half star
+      <div
+        className="star"
+        key={`starWithDecimal`}
+        style={{
+          left: leftPos,
+          height: height,
+          width: decimals * width - spacing,
+        }}
+      ></div>
+    );
+  }
+
+  const starPlaceholder = [];
+  for (let index = 0; index < 5; index++) {
+    starPlaceholder.push(
+      //star placeholder abu"
+      <div
+        className="star placeholder"
+        key={`starPlaceholder-${index}`}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing,
+        }}
+      ></div>
+    );
+  }
+
+  return (
+    <>
+      <div
+        className={["stars", className].join(" ")}
+        style={{ height: height }}
+      >
+        {starPlaceholder}
+        {star}
+      </div>
+    </>
+  );
+}
+
+Star.propTypes = {
+  className: propTypes.string,
+  value: propTypes.number,
+  width: propTypes.number,
+  height: propTypes.number,
+  spacing: propTypes.number,
+};
